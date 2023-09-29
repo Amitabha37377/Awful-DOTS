91dfbf6c26e2954d9899023eb8416dfbd540e720
Amitabha <116963065+Amitabha37377@users.noreply.github.com>
2023-09-29 20:25:00 +0530

    Delete configs/nvim directory

--- configs/nvim/lua/plugins/configs/guess-indent.lua
+++
@@ -1,4 +0,0 @@
-return function(_, opts)
-  require("guess-indent").setup(opts)
-  vim.cmd.lua { args = { "require('guess-indent').set_from_buffer('auto_cmd')" }, mods = { silent = true } }
-end
--- configs/nvim/lua/plugins/configs/luasnip.lua
+++
@@ -1,4 +0,0 @@
-return function(_, opts)
-  if opts then require("luasnip").config.setup(opts) end
-  vim.tbl_map(function(type) require("luasnip.loaders.from_" .. type).lazy_load() end, { "vscode", "snipmate", "lua" })
-end
